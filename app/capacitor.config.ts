ffa354519f6de85aebe450b4ee87865ea55820c9
youdangshi <106019407+youdangshi@users.noreply.github.com>
2026-08-29 20:26:46 +0800

    Upload app/capacitor.config.ts

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nikon.camera.control',
+  appName: 'Nikon Camera Control',
+  webDir: 'dist'
+};
+
+export default config;

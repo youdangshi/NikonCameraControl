b9c4dc6deced8dcccf895bca6c9c3439c26a3055
youdangshi <106019407+youdangshi@users.noreply.github.com>
2026-08-29 20:25:03 +0800

    Upload app/android/app/src/main/java/com/nikon/camera/control/MainActivity.java

---
+++ app/android/app/src/main/java/com/nikon/camera/control/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nikon.camera.control;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(TcpSocketPlugin.class);
+        registerPlugin(UsbPtpPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
